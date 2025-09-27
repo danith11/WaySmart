1aeb22defa8b6db25afb3978e1140d67f30e39d1
Yethum Danith <yethumdanith2002@gmail.com>
2025-09-27 16:51:52 +0530

    Map added

--- src/app/components/mapComponent.jsx
+++ src/app/components/mapComponent.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { MapContainer,TileLayer,Marker,Popup } from "react-leaflet";
 
 const mapComponent = () => {
   return (
     <div>
       <h1>This is mapComponent</h1>
+      <MapContainer>
+        <TileLayer/>
+        <Marker>
+          <Popup>Colombo, Sri Lanka</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
