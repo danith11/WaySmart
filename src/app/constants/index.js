9c1008af7530617f6ed6a916defa8db41a35adbe
Yethum Danith <yethumdanith2002@gmail.com>
2025-11-07 21:17:24 +0530

    href adjusting

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -1,6 +1,6 @@
 export const NAV_LINKS = [
   { href: "/", key: "home", label: "Home" },
-  { href: "/contact-us", key: "contact_us", label: "Contact Us" },
-  { href: "/about", key: "about", label: "About" },
-  { href: "/show-map", key: "show_map", label: "Show Map" },
+  { href: "/app/contact-us", key: "contact_us", label: "Contact Us" },
+  { href: "/app/about", key: "about", label: "About" },
+  { href: "/app/show-map", key: "show_map", label: "Show Map" },
 ];
